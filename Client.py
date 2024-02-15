3e66b9e7ada95ab01566e92436ede6b4dc91e804
xiaohui <xiaohui@XIAO-PC>
2024-02-15 14:52:41 +0100

    first commit

---
+++ Client.py
@@ -0,0 +1,14 @@
+from socket import *
+serverName="127.0.0.1"
+serverPort=12000
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName,serverPort)) 
+while True: 
+        message = input(">")
+        clientSocket.send(message.encode())
+        if message.strip().lower() == 'close':
+            clientSocket.close()  
+            break
+        modifiedMessage = clientSocket.recv(1024)
+        print("Received from server:", modifiedMessage.decode())
+     
