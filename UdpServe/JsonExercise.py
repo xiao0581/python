666f94774ccf09cdbcc843c618f3b967ae7fe602
xiaohui yu <90712496+xiao0581@users.noreply.github.com>
2024-02-25 15:35:44 +0100

    Delete UdpServe directory

--- UdpServe/JsonExercise.py
+++
@@ -1,9 +0,0 @@
-import json
-
-json_string = '{"Name":"Move Cars","Address":"MagleGaardsvej 2","Cars":[{"Brand":"BMW","Model":"330e","Color":"Green","Mileage":45721},{"Brand":"VW","Model":"Golf","Color":"Red","Mileage":20},{"Brand":"Ford","Model":"Galaxy","Color":"Black","Mileage":124326}],"Employees":[{"Name":"Move","Salary":1000000,"MonthsEmployed":28,"JobAreas":["President","Mechanic"]},{"Name":"Not Move","Salary":100,"MonthsEmployed":13,"JobAreas":["Vice-President","Mechanic"]}]}'
-
-# Deserialize JSON string into a dictionary
-data = json.loads(json_string)
-
-# Access the color of the second car
-print(data["Cars"][1]["Color"])
--- UdpServe/echoClient.py
+++
@@ -1,9 +0,0 @@
-from socket import*
-serverName="127.0.0.1"
-ServerPort=8989
-clientScoket=socket(AF_INET,SOCK_DGRAM)
-message=input("input lowercase sentence:")
-clientScoket.sendto(message.encode(),(serverName,ServerPort))
-modifiedMessage, serverAddress = clientScoket.recvfrom(2048)
-print(modifiedMessage.decode())
-clientScoket.close()
